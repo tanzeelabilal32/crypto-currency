b38c90746900df625f6042536dce7f96a153752f
Tanzeela Bilal <tanzeela@jinglepay.com>
2025-03-06 21:40:20 +1100

    Theme support added

--- app/src/main/java/com/crypto/currency/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/crypto/currency/presentation/ui/activity/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.crypto.currency.navigation.NavGraph
+import com.crypto.currency.presentation.ui.theme.CryptoCurrencyRatesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -12,7 +13,9 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavGraph()
+            CryptoCurrencyRatesTheme {
+                NavGraph()
+            }
         }
     }
 }
--- app/src/main/java/com/crypto/currency/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/crypto/currency/presentation/ui/theme/Theme.kt
@@ -10,31 +10,33 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+
+val LightColorScheme = lightColorScheme(
+    primary = OrangePrimary,
+    secondary = Color.Gray,
+    background = LightGrayBackground,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = Color.Gray,
+    background = Color.Black,
+    surface = Color.DarkGray,
     onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    onSecondary = Color.LightGray,
+    onBackground = Color.White,
+    onSurface = Color.White
 )
 
 @Composable
